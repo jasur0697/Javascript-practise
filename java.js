07b3b592d0fce5d2b0206f926e70606927453bcf
jasur0697 <nurmamatov1997@bk.ru>
2020-08-09 14:35:35 +0500

    Update java.js
     added some new practise

--- java.js
+++ java.js
@@ -145,3 +145,21 @@ function showTabsContent(b) {
     }
 }
 
+// #Practise 6 pop up window that closes  
+let modal = document.getElementById("myModal");
+let btn = document.getElementById("myBtn");
+let span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+    modal.style.display = "block"
+}
+
+span.onclick = function() {
+    modal.style.display = "none"
+}
+
+window.onclick = function (event){
+ if (event.target == modal){
+     modal.style.displays ="none"
+ } 
+}
